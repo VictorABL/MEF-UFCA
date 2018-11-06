1e68b315ef87f18ba9f51d23c877c8c8c69c4bc2
VictorABL <victorabl98@gmail.com>
2018-11-06 18:21:51 -0300

    Cleaned up teste.cpp file

--- testes.cpp
+++ testes.cpp
@@ -26,32 +26,7 @@ void Imprimir(const FEM::Matriz<Est, T> &matriz_a) {
 
 int main() {
 
-
-    FEM::Ponto2D<double> p1({1.0,1.0});
-    FEM::Ponto2D<double> p2({-1,11.0});
-    FEM::Ponto2D<double> p3({2,5.6});
-    FEM::Ponto2D<double> p4({2,3});
-    FEM::Ponto2D<double> p5({3,3});
-
     FEM::LinearElastico<double> mat("aco", 200E9, 0.3);
-
-    typedef std::shared_ptr<FEM::No<FEM::Ponto2D, double> > shared_ptr;
-    typedef FEM::No<FEM::Ponto2D, double> ponto_2;
-
-    // FEM::VetorPonteiro<FEM::No<FEM::Ponto2D, double> > v;
-    FEM::ListaPonteiro<FEM::No<FEM::Ponto2D, double> > l;
-
-    l.AdicionarFinal(shared_ptr(new FEM::No<FEM::Ponto2D, double>(p1, 3)));
-    l.AdicionarFinal(shared_ptr(new FEM::No<FEM::Ponto2D, double>(p2, 3)));
-    l.AdicionarFinal(shared_ptr(new FEM::No<FEM::Ponto2D, double>(p3, 3)));
-    l.AdicionarFinal(shared_ptr(new FEM::No<FEM::Ponto2D, double>(p4, 3)));
-
-    FEM::Face<FEM::Ponto2D, double> f1(l);
-    std::shared_ptr<FEM::Face<FEM::Ponto2D, double> > f2(new FEM::Face<FEM::Ponto2D, double>(l));
-
-    // auto e1 = FEM::QuadrilateroIsoparametrico<FEM::LinearElastico, double>(f2, mat, 3E-3);
-    // auto k = e1.MatrizRigidezElementar();
-    // Imprimir(k);
 
     auto a = FEM::Analise<FEM::QuadrilateroIsoparametrico,FEM::LinearElastico, FEM::Ponto2D, double>(8, mat, 3E-3, 3);
 
